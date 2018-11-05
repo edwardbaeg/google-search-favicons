2c440dde0148fb5e92e1ca4e34f1ee9155e3bd8b
edwardbaeg <edwardbaeg9@gmail.com>
2018-11-05 13:40:53 -0800

    Add documentation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 const GOOG_SRC = 'https://www.google.com/s2/favicons?domain=';
 
+/**
+ * Get favicon url
+ * @param {string} site The site url
+ * @returns {string} The favicon url
+ */
 const getFaviconSRC = (site) => {
   const url = new URL(site);
   return GOOG_SRC + url.hostname;
 };
 
+/**
+ * Create favicon img element
+ * @param {HTMLNode} searchResult The result link
+ * @returns {HTMLNode}
+ */
 const createFavicon = (searchResult) => {
   const resultURL = $(searchResult).find('a:first').attr('href');
   const src = getFaviconSRC(resultURL);
