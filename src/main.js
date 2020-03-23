21dc178a638e5fb939731c10165e9a0faee4e43b
edwardbaeg <edwardbaeg9@gmail.com>
2020-03-22 18:31:36 -0700

    Refactor all code

--- src/main.js
+++ src/main.js
@@ -1,52 +1,29 @@
-const FAVICON_SIZE = 16;
-const FAVICON_MARGIN = 6;
+const RESULT_SELECTOR = 'div .rc';
+const FAVICON_API = 'https://www.google.com/s2/favicons?domain=';
+const FAVICON_SIZE = 14;
+const FAVICON_MARGIN = 4;
 
-/**
- * Get favicon url
- * @param {HTMLNode} div The search result div
- * @returns {string} The favicon url
- */
-const getFaviconURL = (div) => {
-  const href = div.querySelector('a').getAttribute('href');
-  const { hostname } = new URL(href);
-  return `https://www.google.com/s2/favicons?domain=${hostname}`;
-};
+void function addFavicons() {
+  // Get all standard search results
+  const results = document.querySelectorAll(RESULT_SELECTOR);
 
-/**
- * Create favicon img element
- * @param {string} src The favicon url
- * @returns {HTMLNode}
- */
-const createFavicon = (src) => {
-  const faviconIMG = new Image(FAVICON_SIZE, FAVICON_SIZE);
-  faviconIMG.src = src;
-  faviconIMG.style.marginRight = `${FAVICON_MARGIN}px`;
-  return faviconIMG;
-};
+  results.forEach(res => {
+    // Get the url and its domain
+    const href = res.querySelector('a').getAttribute('href');
+    const { hostname } = new URL(href);
+    const faviconSrc = FAVICON_API + hostname;
 
-/**
-* Check if Google has rolled out native favicons
-*  for the user's favicons
-*/
-const hasNativeFavicons = (el) => {
-  const link = el.querySelector('a');
-  const target = link.querySelector('img');
-  return !!target;
-}
+    // Create favicon <img> element
+    const el = new Image(FAVICON_SIZE, FAVICON_SIZE);
+    el.src = faviconSrc;
+    el.style.marginRight = FAVICON_MARGIN + 'px';
 
-/**
- * Add favicons to Google search results
- */
-const addFavicons = (() => {
-  document.querySelectorAll('div .rc').forEach(div => {
-    const test = hasNativeFavicons(div);
-    if (test) return;
+    const anchorEl = res.querySelector('cite');
+    anchorEl.style.display = 'flex';
+    anchorEl.style.alignItems = 'center';
 
-    const faviconURL = getFaviconURL(div);
-    const faviconIMG = createFavicon(faviconURL);
-
-    const anchor = div.querySelector('a h3');
-    anchor.prepend(faviconIMG);
-    anchor.style.whiteSpace = 'nowrap';
-  });
-})();
+    const actionMenuEl = res.querySelector('.action-menu');
+    actionMenuEl && (actionMenuEl.style.marginLeft = FAVICON_SIZE + FAVICON_MARGIN + 'px');
+    anchorEl.prepend(el);
+  })
+}();
